78b8fec42c6bc16db7c5a19112e4b48bfc8bd031
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2023-01-17 22:16:48 +0900

    오브젝트 위치 초기화

---
+++ Assets/Scripts/Udon/Object/ResetObject.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ResetObject : UdonSharpBehaviour
+{
+    public Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = transform.position;
+
+        
+    }
+    public void ResetPos()
+    {
+        transform.position = originalPosition;
+        Debug.Log("hello");
+    }
+}
