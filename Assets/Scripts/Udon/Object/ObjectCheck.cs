d1d3f58cbb5e455ae78a20a423dbf7c60600c4aa
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2023-01-18 02:12:32 +0900

    문 애니메이션 추가

---
+++ Assets/Scripts/Udon/Object/ObjectCheck.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ObjectCheck : UdonSharpBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Debug.Log("kinematic " +  rb.isKinematic);
+        Debug.Log("mass" + rb.mass);
+        Debug.Log("drag " + rb.drag);
+        Debug.Log("angularDrag " + rb.angularDrag);
+        Debug.Log("constraints " + rb.constraints);
+    }
+}
