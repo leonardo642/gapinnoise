dfd89f567791555a64ced11c65778a6cfcb0c4fa
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2023-01-20 04:46:53 +0900

    [김기욱] 텔레포트 추가

---
+++ Assets/Scene/VRCScene_UdonProgramSources/Script/Teleport.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Teleport : UdonSharpBehaviour
+{
+    public Transform target;
+
+    public void Move(VRCPlayerApi player)
+    {
+        player.TeleportTo(target.position, target.rotation);
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (player.IsValid())
+        {
+            if (player.isLocal)
+            {
+                Move(player);
+            }
+        }
+
+
+    }
+}
