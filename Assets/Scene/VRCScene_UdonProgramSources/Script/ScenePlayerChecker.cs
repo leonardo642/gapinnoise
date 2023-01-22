93d7d6b3f3b6ec0c6bd1ede033e86b779af3839c
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2023-01-23 03:55:13 +0900

    최적화 작업

---
+++ Assets/Scene/VRCScene_UdonProgramSources/Script/ScenePlayerChecker.cs
@@ -0,0 +1,39 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ScenePlayerChecker : UdonSharpBehaviour
+{
+    public GameObject target;
+
+    void Start()
+    {
+        MeshVisible(false);
+    }
+
+    void MeshVisible(bool visible)
+    {
+        MeshRenderer[] meshes = target.GetComponentsInChildren<MeshRenderer>();
+
+        foreach (var item in meshes)
+        {
+            item.enabled = visible;
+        }
+    }
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (player.isLocal)
+            MeshVisible(true);
+
+
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (player.isLocal)
+            MeshVisible(false);
+
+    }
+}
